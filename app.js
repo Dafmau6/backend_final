782ceec66c621f11031a7066de5d33b4c23c94f1
Dafmau6 <137238831+Dafmau6@users.noreply.github.com>
2023-07-13 19:35:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const connect = require('./config/db');
 const profesoresRoutes = require('./routes/profesores');
-const cors=require('cors');
-const dotenv = require("dotenv");
-
+const cors=require('./config/cors');
 
 const app = express();
 connect();
+app.use(cors);
 app.use(express.json());
 app.use('/profesores', profesoresRoutes);
-app.use(cors());
+
 
 const port = process.env.PORT || 3000;
 app.listen(port,
@@ -17,4 +16,5 @@ app.listen(port,
 corriendo en http://localhost:${3000}`)
 );
 module.exports = app;
+
 
